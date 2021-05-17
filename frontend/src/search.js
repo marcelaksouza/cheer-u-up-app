0ceb7f880acc3b85f638cfa077644e34e0622b4a
marcelaksouza <ma.karoline@hotmail.com>
2021-05-17 14:55:27 +0100

    fix index

--- frontend/src/search.js
+++ frontend/src/search.js
@@ -45,7 +45,7 @@ const buildCardDiv = (cats) => {
         catDisplay += `<div class="card">`;
         catDisplay += `<img src=" ${cat.url}" class="img-fluid" alt="kitty pic" onclick="openModalfunction('${cat.id}')">`;
         catDisplay += `<div id="heartDivId-${cat.id}" class="heart">`;
-        catDisplay += `<img id="catImgId-${cat.id}" src="img/heart1.png" onclick="favoriteFunc('${cat.id}')" width="30" height="30" class="d-inline-block align-top" alt="blankHeart">`
+        catDisplay += `<img id="catImgId-${cat.id}" src="frontend/img/heart1.png" onclick="favoriteFunc('${cat.id}')" width="30" height="30" class="d-inline-block align-top" alt="blankHeart">`
         catDisplay += `</div>`;
         catDisplay += `</div>`;
     });
--- frontend/src/togglefav.js
+++ frontend/src/togglefav.js
@@ -15,14 +15,14 @@ const favoriteFunc = (id) => {
             //if yes
             if (favorite.isFavorite == "true") {
                 //add a blank heart
-                heartDisplay += `<img id="catImgId-${id}" src="img/heart1.png" onclick="favoriteFunc('${id}')" width="30" height="30" class="d-inline-block align-top" alt="blankHeart">`
+                heartDisplay += `<img id="catImgId-${id}" src="frontend/img/heart1.png" onclick="favoriteFunc('${id}')" width="30" height="30" class="d-inline-block align-top" alt="blankHeart">`
                 //delete from favorite
                 deleteFavorite(favorite.id)
             }
             //if no
             else {
                 //add a red heart
-                heartDisplay += `<img id="catImgId-${id}" src="img/heart2.png" onclick="favoriteFunc('${id}')" width="30" height="30" class="d-inline-block align-top" alt="redHeart">`
+                heartDisplay += `<img id="catImgId-${id}" src="frontend/img/heart2.png" onclick="favoriteFunc('${id}')" width="30" height="30" class="d-inline-block align-top" alt="redHeart">`
                 //post to favorite
                 postFavorite(id);
             }
